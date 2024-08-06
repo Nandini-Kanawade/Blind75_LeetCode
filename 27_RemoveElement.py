5dca7f43e4cae0f630de1ae2fe1ba77093cec808
Nandini Kanawade <92260339+Nandini-Kanawade@users.noreply.github.com>
2024-08-07 01:34:39 +0530

    Create 27_RemoveElement.py

---
+++ 27_RemoveElement.py
@@ -0,0 +1,10 @@
+'''
+27. Remove Element
+https://leetcode.com/problems/remove-element/description/
+'''
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+        
