7df6b6c8de4b4bdf7a7f1a7c6fb98c78af0906a5
Nandini Kanawade <92260339+Nandini-Kanawade@users.noreply.github.com>
2024-08-15 23:55:03 +0530

    Create 40_CombinationSumII.py

---
+++ 40_CombinationSumII.py
@@ -0,0 +1,27 @@
+'''
+https://leetcode.com/problems/combination-sum-ii/description/
+40. Combination Sum II
+'''
+
+class Solution:
+  def combinationSum2(self, candidates: list[int],
+                      target: int) -> list[list[int]]:
+    ans = []
+
+    def dfs(s: int, target: int, path: list[int]) -> None:
+      if target < 0:
+        return
+      if target == 0:
+        ans.append(path.copy())
+        return
+
+      for i in range(s, len(candidates)):
+        if i > s and candidates[i] == candidates[i - 1]:
+          continue
+        path.append(candidates[i])
+        dfs(i + 1, target - candidates[i], path)
+        path.pop()
+
+    candidates.sort()
+    dfs(0, target, [])
+    return ans
